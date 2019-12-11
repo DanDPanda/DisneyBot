f131ba92367a7074b1230dc4fdb039ed76a250bc
DanDPanda <dan.v.dinh@gmail.com>
2019-12-10 19:36:54 -0600

    Added test for reserve finally

--- test/commands/reserve.test.js
+++ test/commands/reserve.test.js
@@ -8,49 +8,141 @@ describe("reserve", () => {
   beforeEach(() => {
     sampleMessage = require("../factory/message");
     sampleClient = require("../factory/client");
+    const mockDate = new Date(1466424490000);
+    jest.spyOn(global, "Date").mockImplementation(() => mockDate);
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
   });
 
   test("should add a user to the list", () => {
     const jsonUsers = JSON.stringify({
-      users: []
+      users: [],
+      boba: {}
     });
     fs.readFileSync.mockReturnValue(jsonUsers);
 
     reserve(sampleMessage, sampleClient);
+
+    expect(fs.writeFileSync.mock.calls[0][0]).toEqual("./json/users.json");
+    expect(fs.writeFileSync.mock.calls[0][1]).toEqual(
+      JSON.stringify(
+        {
+          users: [
+            {
+              username: sampleMessage.author.username,
+              startTime: new Date().toLocaleString("en-US", {
+                timeZone: "America/Chicago"
+              })
+            }
+          ],
+          boba: {}
+        },
+        null,
+        2
+      )
+    );
   });
 
   test("should remove a user from the list", () => {
     const jsonUsers = JSON.stringify({
       users: [
         {
-          username: sampleMessage.author.username
+          username: sampleMessage.author.username,
+          startTime: new Date().toLocaleString("en-US", {
+            timeZone: "America/Chicago"
+          })
         }
-      ]
+      ],
+      boba: {}
     });
     fs.readFileSync.mockReturnValue(jsonUsers);
 
     reserve(sampleMessage, sampleClient);
+
+    expect(fs.writeFileSync.mock.calls[0][0]).toEqual("./json/users.json");
+    expect(fs.writeFileSync.mock.calls[0][1]).toEqual(
+      JSON.stringify(
+        {
+          users: [],
+          boba: {}
+        },
+        null,
+        2
+      )
+    );
   });
 
   test("should not add a user to the list because it is filled", () => {
     const jsonUsers = JSON.stringify({
       users: [
         {
-          username: "sampleMessage.author.username"
+          username: "sampleMessage.author.username",
+          startTime: new Date().toLocaleString("en-US", {
+            timeZone: "America/Chicago"
+          })
         },
         {
-          username: "sampleMessage.author.username"
+          username: "sampleMessage.author.username",
+          startTime: new Date().toLocaleString("en-US", {
+            timeZone: "America/Chicago"
+          })
         },
         {
-          username: "sampleMessage.author.username"
+          username: "sampleMessage.author.username",
+          startTime: new Date().toLocaleString("en-US", {
+            timeZone: "America/Chicago"
+          })
         },
         {
-          username: "sampleMessage.author.username"
+          username: "sampleMessage.author.username",
+          startTime: new Date().toLocaleString("en-US", {
+            timeZone: "America/Chicago"
+          })
         }
-      ]
+      ],
+      boba: {}
     });
     fs.readFileSync.mockReturnValue(jsonUsers);
 
     reserve(sampleMessage, sampleClient);
+
+    expect(fs.writeFileSync.mock.calls[0][0]).toEqual("./json/users.json");
+    expect(fs.writeFileSync.mock.calls[0][1]).toEqual(
+      JSON.stringify(
+        {
+          users: [
+            {
+              username: "sampleMessage.author.username",
+              startTime: new Date().toLocaleString("en-US", {
+                timeZone: "America/Chicago"
+              })
+            },
+            {
+              username: "sampleMessage.author.username",
+              startTime: new Date().toLocaleString("en-US", {
+                timeZone: "America/Chicago"
+              })
+            },
+            {
+              username: "sampleMessage.author.username",
+              startTime: new Date().toLocaleString("en-US", {
+                timeZone: "America/Chicago"
+              })
+            },
+            {
+              username: "sampleMessage.author.username",
+              startTime: new Date().toLocaleString("en-US", {
+                timeZone: "America/Chicago"
+              })
+            }
+          ],
+          boba: {}
+        },
+        null,
+        2
+      )
+    );
   });
 });
